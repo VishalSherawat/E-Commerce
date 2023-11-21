e1402c2a252e7d7777bd0334dc353a660f264cf1
vishalsherawat <vishalsherawt@gmail.com>
2023-11-22 03:31:29 +0530

    Added support for mobile view

--- client/src/pages/ProductList.jsx
+++ client/src/pages/ProductList.jsx
@@ -5,6 +5,7 @@ import Announcement from "../components/Announcement";
 import Products from "../components/Products";
 import Newsletter from "../components/Newsletter";
 import Footer from "../components/Footer";
+import { mobile } from "../responsive";
 
 const Container = styled.div``;
 
@@ -19,12 +20,14 @@ const FilterContainer = styled.div`
 
 const Filter = styled.div`
   margin: 20px;
+  ${mobile({ margin: "0px 10px", display: "flex", flexDirection: "column", gap: "10px" })}
 `;
 
 const FilterText = styled.span`
   font-size: 20px;
   font-weight: 600;
   margin-right: 20px;
+  ${mobile({ marginRight: "10px" })}
 `;
 
 const Select = styled.select`
